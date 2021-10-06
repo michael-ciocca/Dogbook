fd49ceb5c03d787ca9354009df20498edef359c8
Michael Ciocca <mciocca89@gmail.com>
2021-10-05 20:54:15 -0400

    add paginate for dogs

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -12,4 +12,17 @@ RSpec.feature 'Home page', type: :feature do
     expect(page).to have_text('Sign in')
     expect(page).to have_text('Sign up')
   end
+
+  scenario 'More than 5 dogs exist' do
+    10.times { create(:dog) }
+
+    visit '/'
+
+    expect(page).to have_selector('.pagination .next a')
+
+    click_link('Next ›')
+
+    expect(page).to have_selector('.dog-photo', count: 5)
+    expect(page).to have_selector('.dog-name', count: 5)
+  end
 end
