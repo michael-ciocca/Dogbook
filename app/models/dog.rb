4531fd676e9d858899285ed160ae47220173d9a8
Michael Ciocca <mciocca89@gmail.com>
2021-10-06 21:46:07 -0400

    create relations for dog likes

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,4 +4,6 @@ class Dog < ApplicationRecord
   # optional: true prevents problems with dogs created before this association exsits
   # you probably wouldn't want this optional in a new application
   belongs_to :owner, class_name: "User", foreign_key: "user_id", inverse_of: :dogs, optional: true
+  has_many :likes
+  has_many :admirers, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :dogs, inverse_of: :owner
+  has_many :likes
+  has_many :liked_dogs, through: :likes, source: :dog
 end
